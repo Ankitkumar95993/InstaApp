4a8c7decaceccd45e44bfd4ed7c3400f93e09971
Ankit Kumar <ankitkumar95993@gmail.com>
2024-05-04 12:11:53 +0530

    created the navbar

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import Link from 'next/link'\
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='shadow-sm border-b sticky top-0 bg-black z-index-30 p-2' >
+        <div className='flex items-center justify-between max-w-6xl mx-auto'>
+            {/* logo */}
+
+            <Link href='/' className='hidden lg:inline-flex' > 
+              <Image src='/pic2.webp' width={96} height={96} alt='instagramLogo' className='bg-red-500 rounded px-4 py-2'/>
+            </Link>
+
+            <Link href='/' className='lg:hidden' > 
+              <Image src='/pic1.webp' width={40} height={40} alt='instagramLogo'/>
+            </Link>
+
+            {/* searchInput */}
+
+            <input type="text" placeholder='search' className='bg-gray-50 border
+             border-gray-300 rounded text-sm w-full py-2 px-4 max-w-[210px]'/>
+
+            {/* menuItem */}
+
+            <button className='text-blue-500 text-sm font-bold'>Log In</button>
+
+
+        </div>
+
+    </div>
+  )
+}
